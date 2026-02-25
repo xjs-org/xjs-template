7362e6740906240483a1442bcd8a44f3f29fe53d
aprajayverma <aprajay.v@gmail.com>
2026-02-25 13:12:13 +0530

    base webapp template initialized.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const { htmlElements, svgElements, createSignal, appendElements } = XJS;
+import { App } from "./xjs-components.js";
+
+const webApp = () => {
+    return "Hello World"
+}
+
+export default webApp;
